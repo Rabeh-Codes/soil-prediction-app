3cf172b52956aa09648249fc25f0f45127f746b1
Rabeh-Codes <rabahrabeh565@.com>
2025-07-23 19:12:11 +0100

    fix: correct @typescript-eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,31 @@
+// eslint.config.js
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import * as tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 import { globalIgnores } from 'eslint/config'
 
-export default tseslint.config([
+export default [
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
+      parser: tsParser,
       ecmaVersion: 2020,
       globals: globals.browser,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...reactHooks.configs['recommended'].rules,
+      ...reactRefresh.configs.vite.rules,
     },
   },
-])
+]
